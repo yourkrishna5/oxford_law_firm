93febd4c7717be2d0748c187c56fcab88e1df92b
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-09-28 07:50:24 +0545

    Update apps.py

--- main/apps.py
+++ main/apps.py
@@ -1,23 +1,22 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 from django.contrib.auth.models import User
-from django.db.utils import OperationalError
-
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
 
     def ready(self):
-        try:
-            if not User.objects.filter(username="oxfordform").exists():
-                User.objects.create_superuser(
-                    username="oxfordform",
-                    password="oxford@1",
-                    email="oxfordlawfirm5@gmail.com"
-                )
-                print("✅ Superuser 'oxfordform' created")
-        except OperationalError:
-            # Happens when DB is not migrated yet
-            pass
-        except Exception as e:
-            print("⚠️ Skipped superuser creation:", e)
+        post_migrate.connect(create_admin_user, sender=self)
+
+def create_admin_user(sender, **kwargs):
+    try:
+        if not User.objects.filter(username="oxfordform").exists():
+            User.objects.create_superuser(
+                username="oxfordform",
+                password="oxford@1",
+                email="oxfordlawfirm5@gmail.com"
+            )
+            print("✅ Superuser 'oxfordform' created")
+    except Exception as e:
+        print("⚠️ Skipped superuser creation:", e)
