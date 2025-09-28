a8315c3aaf6967499e61cfd3b4add1d853e0cf5a
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-09-28 08:33:18 +0545

    Update urls.py

--- a/urls.py
+++ a/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from new import urls
+from main import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
