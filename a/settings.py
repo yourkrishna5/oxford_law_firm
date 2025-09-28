08e7b89d3e08d8fb3e266cb0ba5f43cadf9619cb
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-09-28 14:55:16 +0545

    Update settings.py

--- a/settings.py
+++ a/settings.py
@@ -27,6 +27,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+"whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "a.urls"
@@ -77,3 +78,5 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
